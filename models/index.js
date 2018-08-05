ee63160fd7ae5f25bec280d0d41c273b1ccb7a82
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-05 13:40:34 -0700

    Update db name

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = process.env.MONGODB_URI || "mongodb://localhost/chess-pro"
+const dbURI = process.env.MONGODB_URI || "mongodb://localhost/astrocore"
 
 mongoose.connect(dbURI);
 
