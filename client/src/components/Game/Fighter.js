56f4980ba7de4f73bdf677cc77cfaa11d4f887bb
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-08 16:07:42 -0700

    Create Fighter class

---
+++ client/src/components/Game/Fighter.js
@@ -0,0 +1,14 @@
+class Fighter {
+  constructor(id, x, y, score, color, owner) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.color = color;
+    this.owner = owner;
+  }
+}
+
+module.exports = {
+  Fighter,
+};
