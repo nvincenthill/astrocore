10d7510c0b4b122363dd659cabebc998094da56e
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-19 15:27:17 -0700

    Add socket.io

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
-const express = require("express");
-const routes = require("./../routes");
-const path = require("path");
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+const routes = require('./../routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server).listen(server);
 
-app.set("port", process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8080);
 
-app.use(express.static("public/"));
-app.use(express.static("client/dist"));
+app.use(express.static('public/'));
+app.use(express.static('client/dist'));
 
-app.use("/api", routes);
+// routing
+app.use('/api', routes);
+
+// Add the WebSocket handlers
+io.on('connection', (socket) => {});
+
+// testing
+setInterval(() => {
+  io.sockets.emit('message', 'hi!');
+}, 1000);
 
 module.exports = app;
