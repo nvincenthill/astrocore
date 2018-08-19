9cf8c28583daf37dc5fb483bb4d67b2843b44c01
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-08-19 16:30:02 -0700

    Implement socket.io server

--- server/app.js
+++ server/app.js
@@ -1,27 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-const http = require('http');
-const path = require('path');
-const socketIO = require('socket.io');
+// const http = require('http');
+// const path = require('path');
 const routes = require('./../routes');
 
 const app = express();
-const server = http.Server(app);
-const io = socketIO(server).listen(server);
+// const server = http.Server(app);
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.SERVERPORT || 8080);
 
 app.use(express.static('public/'));
 app.use(express.static('client/dist'));
-
 // routing
 app.use('/api', routes);
-
-// Add the WebSocket handlers
-io.on('connection', (socket) => {});
-
-// testing
-setInterval(() => {
-  io.sockets.emit('message', 'hi!');
-}, 1000);
-
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
-const app = require("./app");
+const socketIO = require('socket.io');
+const app = require('./app');
 
-app.listen(app.get("port"), () =>
-  console.log(`listening on port ${app.get("port")}...`)
-);
+const io = socketIO();
+
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+});
+
+const port = process.env.SOCKETPORT || 8000;
+io.listen(port);
+console.log(`socket.io listening on port ${port}...`);
+
+app.listen(app.get('port'), () => console.log(`express server listening on port ${app.get('port')}...`));
