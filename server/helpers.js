86c76e7f24a4135b03136387c59b8e5bc8855192
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-10-05 14:44:56 -0700

    Bugfix fighters for responsive design

--- server/helpers.js
+++ server/helpers.js
@@ -1,11 +1,10 @@
 const validateNodeClicked = (node, x, y) => {
-  const clickRadius = 0.1;
+  const clickRadius = 0.05;
   if (
     y < node.y + clickRadius
     && y > node.y - clickRadius
     && (x < node.x + clickRadius && x > node.x - clickRadius)
   ) {
-    console.log('node was clicked');
     return true;
   }
   return false;
